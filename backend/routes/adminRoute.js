5772c6493db48149ca02b8b2ae0e2e4b6d4f8c79
Abhishek Kumar <abhishekdatapur@gmail.com>
2026-04-25 15:28:42 +0530

    added vercel config

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { getMe, login, logout, register } from '../controller/adminController.js'
-import { isAuthenticated } from '../controller/middleware/adminMiddleware.js'
+import { isAuthenticated } from '../middleware/adminMiddleware.js'
 const adminRouter = express.Router()
 
 adminRouter.post("/register", register)
