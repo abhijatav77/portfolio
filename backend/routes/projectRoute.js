bf380bbd1f7496aa149c65cc158d58f33434ff2c
Abhishek Kumar <abhishekdatapur@gmail.com>
2026-04-25 17:36:40 +0530

    update the project

--- backend/routes/projectRoute.js
+++ backend/routes/projectRoute.js
@@ -4,7 +4,7 @@ import { contact } from '../controller/contactController.js'
 const router = express.Router()
 
 router.post('/project-upload', projectUpload)
-router.put('/project-update:/id', updateProject)
+router.put('/project-update/:id', updateProject)
 router.get('/all-projects', getProject)
 router.get('/single-projects/:id', singleProject)
 router.delete('/project-delete/:id', deleteProject)
