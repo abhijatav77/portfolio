0df71aec91c5598607fe9c9efc0339834d8393fe
Abhishek Kumar <abhishekdatapur@gmail.com>
2026-04-26 17:50:22 +0530

    update the update page

--- frontend/src/Project/Update.jsx
+++ frontend/src/Project/Update.jsx
@@ -118,7 +118,18 @@ const Update = () => {
             </div>
             <div className='mt-4 text-white'>
               <img src={projectImagePreview ? `${projectImagePreview}` : "/imgPL.webp"} alt="Project Image" className='w-full h-30 border border-gray-400 rounded-md' />
-              <input type="file" onChange={(e) => setProjectImage(e.target.files[0])} className='border border-gray-400 rounded-md mt-4 w-full px-4 py-2' />
+              <input 
+                type="file" 
+                onChange={(e) => {
+                  const file = e.target.files[0]
+                  setProjectImage(file)
+                  
+                  if(file){
+                    setProjectImagePreview(URL.createObjectURL(file))
+                  }
+                }}
+                className='border border-gray-400 rounded-md mt-4 w-full px-4 py-2' 
+              />
             </div>
             <button
               type="submit"
